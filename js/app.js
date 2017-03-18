d5da827f4bd35020b7924fb51c2cf1f936d5981b
Andy Walpole <me@andywalpole.me>
2017-03-18 19:18:52 +0000

    changed url for todoMVCPolymer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 (function (window) {
 	'use strict';
 
+	window.addEventListener('load', () => {
+
+		if (window.location.origin !== 'https://tcotton.github.io') {
+
+			console.dir(document.links);
+
+			for (let i = 0; i < document.links.length; i++) {
+				console.log(document.links[i].href);
+				// document.links[i].href += "?name=fc"
+			}
+
+		}
+
+	});
+
 	// Your starting point. Enjoy the ride!
 
 })(window);
